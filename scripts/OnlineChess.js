95886861decb34c4eecc5b6c3b3a41e0ae5e3d18
AMAR <amar@AMARs-MacBook-Pro.local>
2023-04-01 10:14:45 +0530

    updates

--- scripts/OnlineChess.js
+++ scripts/OnlineChess.js
@@ -9,19 +9,19 @@ CHESSAPP.onlinePlay = {
     this.sk = io.connect(hostPort);
     CHESSAPP.ui.statusUpdate({ type: "fb", msg: "Searching for player 1..." });
     this.sk.emit("setup", { color: stg.preferredColor });
-    this.sk.on("chat", function (data) {
+    this.sk.on("chat", { transports: ["websocket"] }, function (data) {
       CHESSAPP.GamePlay.chatMessage(data);
     });
-    this.sk.on("partnerDisconnect", function () {
+    this.sk.on("partnerDisconnect", { transports: ["websocket"] }, function () {
       CHESSAPP.GamePlay.statusUpdate({ type: "e", msg: "Your partner has left the game" });
     });
-    this.sk.on("disconnect", function () {
+    this.sk.on("disconnect", { transports: ["websocket"] }, function () {
       CHESSAPP.GamePlay.statusUpdate({
         type: "e",
         msg: "Internal Server Error. We will be back soon. Sorry for the inconvenience.",
       });
     });
-    this.sk.on("matchfound", function (data) {
+    this.sk.on("matchfound", { transports: ["websocket"] }, function (data) {
       CHESSAPP.GamePlay.statusUpdate({
         type: "fb",
         msg: "Player 2 has joined the game. You can start the game",
@@ -33,7 +33,7 @@ CHESSAPP.onlinePlay = {
       CHESSAPP.GamePlay.setOnlineColor(data.color);
       callback();
     });
-    this.sk.on("opposing_move", function (data) {
+    this.sk.on("opposing_move", { transports: ["websocket"] }, function (data) {
       CHESSAPP.GamePlay.onlineMove(data);
       CHESSAPP.GamePlay.statusUpdate({ type: "s", msg: "It's your move!" });
     });
